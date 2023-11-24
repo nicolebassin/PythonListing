40eeb9774bc5c58b0dfef5766b100036c4a89e69
nicolebassin <nicoleb2512@gmail.com>
2023-11-24 12:44:53 +0200

    Add functions

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Test")
+def print1to100():
+    for i in range(1.101):
+        print(i, end = " | ")
+
+def print100to1():
+    for i in range(100, 0, -1):
+        print(i, end = " | ")
+
+print100to1()
+        
